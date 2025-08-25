df7c4b7c8b9012d241e475f1efd04f94fee78752
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-08-25 22:58:41 +0800

    Melhorando minha applicacao, CRUD de gerenciamento de restaurantes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+restaurantes = []
 
 def exibir_nome_do_programa():
     print("""
@@ -19,15 +20,40 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 
-def opcao_invalida():
-    print('Opcao invalida\n')
-    input('Digite alguma tecla para reiniciar o programa')
-    main()
-
-
 def finalizar_app():
     os.system('cls')
     print('Finalizando app\n')
+
+
+def retornar_para_menu_principal():
+    input('\nDigite qualquer tecla para voltar para o menu principal.\n')
+    main()
+
+
+def opcao_invalida():
+    print('Opcao invalida\n')
+    retornar_para_menu_principal()
+
+
+def imprimir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+
+def cadastrar_restaurante():
+    imprimir_subtitulo('Cadastrando restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que voce deseja cadastrar: ')
+    restaurantes.append(nome_restaurante)
+    print(f'Restaurante {nome_restaurante}, foi cadastrado com sucesso.')
+    retornar_para_menu_principal()
+
+
+def listar_restaurantes():
+    imprimir_subtitulo('Listando restaurantes')
+    for restaurante in restaurantes:
+        print(restaurante)
+    retornar_para_menu_principal()
 
 
 def escolher_opcoes():
@@ -35,9 +61,9 @@ def escolher_opcoes():
         opcao_escolhida = int(input('Escolha uma opcao: '))
         # print(f'Voce escolheu a opcao: {opcao_escolhida}')
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadastrar_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurante')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
@@ -59,3 +85,8 @@ def main():
 
 if __name__ ==  '__main__':
     main()
+
+
+# criar funcao cadastrar restaurantes
+# criar funcao listar restaurantes
+# criar funcao que retorna ao menu principal
